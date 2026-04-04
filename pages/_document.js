846e894b165cab05411779db1ce18deffc789038
jose am talavera <115177988+joseamtalavera@users.noreply.github.com>
2026-04-04 19:32:20 +0200

    Merge gtag and GTM into single script to prevent hydration stripping

--- pages/_document.js
+++ pages/_document.js
@@ -10,19 +10,11 @@ export default class MyDocument extends Document {
         <Head>
           <script
             dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              __html: `window.dataLayer=window.dataLayer||[];window.gtag=function(){dataLayer.push(arguments);};(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-T5BD4L36');`,
-            }}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                window.gtag = function(){dataLayer.push(arguments);};
-              `,
             }}
           />  
           <link rel="icon" href="/new_favicon.png" />
